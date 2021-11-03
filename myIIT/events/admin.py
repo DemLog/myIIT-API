49a642bff77624cc4ad451ead41d8fa08d9a51e7
DemLoveSky <dem.loginov@gmail.com>
2021-11-04 02:45:46 +0500

    Создана модель Event

--- myIIT/events/admin.py
+++ myIIT/events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author']
--- myIIT/events/models.py
+++ myIIT/events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    CHOICES = (
+        ('W', 'Ожидание публикации'),
+        ('P', 'Опубликовано'),
+        ('R', 'Отклонено'),
+        ('C', 'Завершено')
+    )
+    title = models.CharField(verbose_name='Название', max_length=128)
+    description = models.CharField(verbose_name='Описание', max_length=256)
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.SET_NULL, null=True, related_name='event')
+    votes = models.ManyToManyField(User, verbose_name='Проголосовавшие', blank=True, related_name='event_votes')
+    date_create = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    status = models.CharField(verbose_name='Статус', max_length=2, choices=CHOICES, default='W')
+    report = models.CharField(verbose_name='Сообщение', max_length=256, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'мероприятие'
+        verbose_name_plural = 'мероприятия'
